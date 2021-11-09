1db24c6b286c8183a833266c4a581a910658d823
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-11-10 00:28:18 +0500

    first commit

---
+++ lib/dialog.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Dialogclass extends StatelessWidget {
+  const Dialogclass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ElevatedButton(
+              onPressed: () {
+                Get.defaultDialog();
+              },
+              child: Text("Snackbar")),
+        ]),
+      )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getxpractice/snackbar.dart';
+import 'package:getxpractice/dialog.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Snackbar();
+    return Dialogclass();
   }
 }
